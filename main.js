58a98476d3ab3b76e94bc6527c8361c0709778fa
LiliyaShvets <lilianashv@gmail.com>
2021-04-13 19:06:47 +0300

    elearndev
    elearndev

---
+++ main.js
@@ -0,0 +1,8 @@
+$('.multiple-items').slick({
+  infinite: true,
+  dots: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-prev.svg" alt=""></img>',
+  nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-next.svg" alt=""></img>'
+});
