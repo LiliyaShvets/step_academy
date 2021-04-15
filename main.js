cfaef271f40c8d881fe3b6ded579ad54c4159c6c
LiliyaShvets <lilianashv@gmail.com>
2021-04-15 22:47:03 +0300

    mobile

--- main.js
+++ main.js
@@ -4,5 +4,19 @@ $('.multiple-items').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/arrow-prev.svg" alt=""></img>',
-  nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-next.svg" alt=""></img>'
+  nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/arrow-next.svg" alt=""></img>',
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 2,
+      }
+    },
+    {
+      breakpoint: 920,
+      settings: {
+        slidesToShow: 1,
+      }
+    }
+  ]
 });
